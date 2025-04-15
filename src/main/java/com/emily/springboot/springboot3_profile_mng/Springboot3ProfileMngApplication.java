0d58a0ab2042f6c15bc65efefe7e10e2896def39
Emily Amorim <amorimemily@gmail.com>
2025-04-15 09:14:41 -0300

    Removendo .gitignore do versionamento

---
+++ src/main/java/com/emily/springboot/springboot3_profile_mng/Springboot3ProfileMngApplication.java
@@ -0,0 +1,13 @@
+package com.emily.springboot.springboot3_profile_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3ProfileMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3ProfileMngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emily/springboot/springboot3_profile_mng/Springboot3ProfileMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emily.springboot.springboot3_profile_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ProfileMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
